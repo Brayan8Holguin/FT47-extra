5180929bff420915033e3cdb1a4a233f9342ed88
HX-FMontoya <felipeciromontoya.developer@gmail.com>
2024-02-19 12:54:58 -0500

    add hw rym async await done

--- rym/back/src/handlers/getCharById_API.js
+++ rym/back/src/handlers/getCharById_API.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const { API_KEY, API_URL_BASE } = process.env;
 
-function getCharById_API(id) {
+//? con promesas
+/* function getCharById_API(id) {
   // "https://rym2.up.railway.app/api/character" + "/" + "1" + "?key={}"
   // id, status, name, species, origin, image y gender
-  return fetch(`${API_URL_BASE}/${id}?key=${API_KEY}`)
+  const url = `${API_URL_BASE}/${id}?key=${API_KEY}`;
+  return fetch(url)
     .then((res) => res.json())
     .then((data) => {
       const character = {
@@ -21,6 +23,28 @@ function getCharById_API(id) {
     .catch((err) => {
       return err;
     });
+} */
+  
+//? con async await
+  
+async function getCharById_API(id) { 
+  try {
+    const url = `${API_URL_BASE}/${id}?key=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const character = {
+      id: data.id,
+      name: data.name,
+      status: data.status,
+      species: data.species,
+      origin: data.origin.name,
+      image: data.image,
+      gender: data.gender,
+    };
+    return character;
+  } catch (error) {
+    return error;
+  }
 }
 
 module.exports = getCharById_API;
