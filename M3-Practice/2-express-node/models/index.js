713f79194c2e0116ed473ff83e7483e2d7aff02f
HX-FMontoya <felipeciromontoya.developer@gmail.com>
2024-02-19 11:12:53 -0500

    hw express node done

---
+++ M3-Practice/2-express-node/models/index.js
@@ -0,0 +1,9 @@
+const products = [];
+// product -> { id: 1, name: 'product 1', price: 100 }
+const users = [];
+// user -> { id: 1, name: 'user 1' , email: 'email@email.com'}
+
+module.exports = {
+  products,
+  users,
+};
