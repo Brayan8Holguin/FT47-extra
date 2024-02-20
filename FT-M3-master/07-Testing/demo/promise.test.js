6d0848ec82c568bc65e5a68ae565c16d57b62d30
HX-FMontoya <felipeciromontoya.developer@gmail.com>
2024-02-20 16:33:26 -0500

    add demo testing

--- FT-M3-master/07-Testing/demo/promise.test.js
+++ FT-M3-master/07-Testing/demo/promise.test.js
@@ -1,36 +1,41 @@
 const promisifiedFunction = require('./promise');
 
-xit('should resolve to Henry Promise', () => {
+/* it('should resolve to Henry Promise', () => {
   return promisifiedFunction(true).then(data => {
     expect(data).toBe('Henry Promise');
   });
 });
 
-xit('should resolve to Henry Promise (other way)', () => {
-  return expect(promisifiedFunction(true)).resolves.toBe('Henry Promise');
-});
+it("should resolve to Henry Promise", async () => {
+  const result = await promisifiedFunction(true);
+  expect(result).toBe("Henry Promise");
+}); */
 
-xit('should resolve to Henry Promise (async/await)', async () => {
+/* it('should resolve to Henry Promise (other way)', () => {
+  return expect(promisifiedFunction(true)).resolves.toBe('Henry Promise');
+}); */
+
+/* xit('should resolve to Henry Promise (async/await)', async () => {
   const data = await promisifiedFunction(true);
   expect(data).toBe('Henry Promise');
-});
+}); */
 
-xit('should reject to Rejected Promise', () => {
+/* xit('should reject to Rejected Promise', () => {
   expect.assertions(1);
   return promisifiedFunction(false).catch(e => {
     expect(e).toMatch('Rejected Promise')
   });
-});
+}); */
 
-xit('should reject to Rejected Promise (other way)', () => {
+/* xit('should reject to Rejected Promise (other way)', () => {
   return expect(promisifiedFunction(false)).rejects.toMatch('Rejected Promise');
-});
+}); */
 
-xit('should reject to Rejected Promise (async/await)', async () => {
+/* xit('should reject to Rejected Promise (async/await)', async () => {
   expect.assertions(1);
   try {
     await promisifiedFunction(false);
   } catch (error) {
     expect(error).toMatch('Rejected Promise');
   }
-});
+}); */
